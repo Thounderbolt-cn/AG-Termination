a03b2fc590f5d12f6cbfc3a5b0d5b056f024b05b
heisenberg-wf <55394918+heisenberg-wf@users.noreply.github.com>
2019-09-22 10:36:04 +0800

    upgrade

---
+++ termination-web/src/main/java/com/aplus/gaming/web/controller/MainController.java
@@ -0,0 +1,21 @@
+package com.aplus.gaming.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jarvis
+ * @version V1.0
+ * @@desc: TODO
+ * @date 2019/9/22 0022
+ **/
+@Controller
+@RestController(value = "server")
+public class MainController {
+
+    @GetMapping("/checkStatus")
+    public String checkStatus(){
+        return "SUCCESS";
+    }
+}
