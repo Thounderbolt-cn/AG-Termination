184cf40694335d7cb4621f3fbf68fb962799fa1e
heisenberg-wf <55394918+heisenberg-wf@users.noreply.github.com>
2019-10-04 11:47:49 +0800

    mod port

--- termination-web/src/main/java/com/aplus/gaming/web/Application.java
+++ termination-web/src/main/java/com/aplus/gaming/web/Application.java
@@ -2,13 +2,13 @@ package com.aplus.gaming.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 启动类
+ *
  * @author Javis
  * @date 2019年6月17日08:35:24
  */
@@ -21,14 +21,7 @@ public class Application {
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
-
-        ApplicationContext applicationContext =
-                SpringApplication.run(Application.class, args);
-
-//        for (String name : applicationContext.getBeanDefinitionNames()) {
-//            System.out.println(name);
-//        }
-
-        System.out.println("Server startup expense time:"+ (System.currentTimeMillis() - start) + "ms." );
+        SpringApplication.run(Application.class, args);
+        System.out.println("Server startup expense time:" + (System.currentTimeMillis() - start) + "ms.");
     }
 }
